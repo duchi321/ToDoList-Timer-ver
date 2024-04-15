2ec3a271b07572a418d18c69cbcb13479f94e444
duchi321 <sqaz0502@gmail.com>
2024-04-15 18:28:15 +0800

    add removeitem & none function & rename section className

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const currentTime = document.querySelector('#currentTime')
 const newTodo = document.querySelector('#new-todo')
 const addBtn = document.querySelector('#add-btn')
 const myTodo = document.querySelector('#my-todo')
+const myDone = document.querySelector('#done')
+const checkList = document.querySelector('#checkList')
+
 
 const model = {
     getdataDate() {
@@ -21,6 +24,16 @@ const view = {
         const timeString = `${year}<span>年</span> ${month}<span>月</span> ${day}<span>日</span> ${hours}:${minutes}:${seconds}`;
         currentTime.innerHTML = timeString;
     },
+    renderNone() {
+        let warning = document.createElement('div')
+        warning.classList.add('warning')
+        warning.textContent = 'none...'
+        myTodo.prepend(warning)
+    },
+    renderRemoveNone() {
+        let warning = document.querySelector('.warning')
+        warning.remove()
+    },
     renderNewItem(text) {
         let newItem = document.createElement('li')
         newItem.innerHTML = `
@@ -28,6 +41,9 @@ const view = {
         <i class="delete">X</i>
         `
         myTodo.appendChild(newItem)
+    },
+    renderRemoveItem(click) {
+        click.remove()
     }
 }
 
@@ -36,17 +52,44 @@ const controller = {
         setInterval(view.renderCurrentTime, 1000)
         view.renderCurrentTime()
     },
+    addNone() {
+        if (myTodo.childElementCount === 0) {
+            view.renderNone()
+        }
+    },
+    removeNone() {
+        let childElements = myTodo.querySelectorAll("*");
+        // 遍歷每個子元素，檢查是否與指定的 CSS 選擇器匹配
+        for (let i = 0; i < childElements.length; i++) {
+            let childElement = childElements[i];
+            if (childElement.matches(".warning")) {
+                view.renderRemoveNone()
+            }
+        }
+    },
     getNewItem() {
         addBtn.addEventListener('click', () => {
             let getText = newTodo.value.trim()
             if (getText.length > 0) {
+                this.removeNone()
                 view.renderNewItem(getText)
                 newTodo.value = ''
             } else {
                 alert('請輸入內容')
             }
         })
+    },
+    removeItem() {
+        checkList.addEventListener('click', (evnet) => {
+            if (evnet.target.matches('.delete')) {
+                view.renderRemoveItem(evnet.target.parentElement)
+                this.addNone()
+            }
+        })
     }
 }
+
+controller.addNone()
 controller.getCurrentTime()
 controller.getNewItem()
+controller.removeItem()
